7ab216e200a9901f6765280d0de6add6333faf2c
shaoyiya <993170145@qq.com>
2024-07-20 15:38:12 +0800

    vconsole

--- src/main.js
+++ src/main.js
@@ -5,5 +5,12 @@ import App from './App.vue'
 import 'normalize.css/normalize.css'
 import router from './router'
 import pinia from './store'
+// import Vconsole from 'vconsole'
+
+console.log(import.meta.env)
+// if(import.meta.env.VITE_ENV !== 'production') {
+//     // 测试和开发环境打开
+//     new Vconsole();
+// }
 
 createApp(App).use(router).use(pinia).mount('#app')
